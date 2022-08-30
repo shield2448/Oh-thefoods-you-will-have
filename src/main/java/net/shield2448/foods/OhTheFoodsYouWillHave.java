cec87dfa5b46fe3b59d42a2cfe2d4a952f9667a3
SRINJAYDASGUPTA-Git <dasguptasrinjay2004@gmail.com>
2022-08-30 10:21:03 +0530

    initial setup

---
+++ src/main/java/net/shield2448/foods/OhTheFoodsYouWillHave.java
@@ -0,0 +1,17 @@
+package net.shield2448.foods;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OhTheFoodsYouWillHave implements ModInitializer {
+
+	public static final String MOD_ID = "foods";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/shield2448/foods/OhTheFoodsYouWillHaveClient.java
@@ -0,0 +1,11 @@
+package net.shield2448.foods;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OhTheFoodsYouWillHaveClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
